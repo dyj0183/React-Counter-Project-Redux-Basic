af0a8aab0d3598cd93abc346a88fbb39a6924016
Yu-Chun Tung <dyj0183@byui.edu>
2022-02-19 20:45:55 -0700

    set up the Provider for the entire project to be able to use Redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+// import the provide in order to use Redux
+import { Provider } from "react-redux";
+// use the Redux store here
+import store from "./store/index";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// we want the entire project to be able to use Redux, so we need to provide at the highest level of the project which is here
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+);
