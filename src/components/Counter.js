ab39ff77a57fa1c6bab93eddc34fc507e117f271
Yu-Chun Tung <dyj0183@byui.edu>
2022-02-20 18:11:11 -0700

    subscribe to the store to get counter value

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,18 @@
 import classes from './Counter.module.css';
+// import this to use Redux here
+import { useSelector } from 'react-redux';
 
 const Counter = () => {
+  // useSelector will give me the most updated counter whenever the value gets updated
+  // subscribe the store to get data
+  const counter = useSelector(state => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
